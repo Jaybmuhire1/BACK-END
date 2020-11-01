7aaf9b111b1b19bed65c17f4c0247ac116e4d72c
Jaybmuhire1 <68017634+Jaybmuhire1@users.noreply.github.com>
2020-11-01 13:31:59 +0200

    small modification

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-
 import app from './app';
 
 
 const PORT = process.env.PORT || 7000;
 
-const server = app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
 
